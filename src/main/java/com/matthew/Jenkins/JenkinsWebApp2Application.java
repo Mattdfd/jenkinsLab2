329191c746807491f0ce46f706725694d237974a
Matthew Mikhaiel <MattDFD@live.ca>
2023-03-04 04:14:05 +0100

    second

---
+++ src/main/java/com/matthew/Jenkins/JenkinsWebApp2Application.java
@@ -0,0 +1,13 @@
+package com.matthew.Jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWebApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsWebApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthew/Jenkins/JenkinsWebApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthew.Jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsWebApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
